9098eeecae37ed2f979c7665f800144f27003cbd
jack <jack@wizmacau.com>
2023-07-04 14:01:07 +0800

    feat: Add global package with initialization function

---
+++ pkg/global/global.go
@@ -0,0 +1,49 @@
+package global
+
+import (
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	storage "go-proxypool/pkg/storage"
+)
+
+var (
+	Storage storage.Storage
+	Logger  *log.Logger
+	Config  *viper.Viper
+)
+
+func Initialize() {
+	Logger = log.New()
+	Logger.SetLevel(log.DebugLevel)
+	Logger.SetFormatter(&log.TextFormatter{})
+
+	Config = viper.New()
+	Config.AutomaticEnv()
+	Config.SetEnvPrefix("PROXYPOOL")
+	Config.SetConfigName("config")
+	Config.SetConfigType("yaml")
+	Config.AddConfigPath("/etc/proxypool")
+	Config.AddConfigPath("$HOME/.proxypool")
+	Config.AddConfigPath(".")
+	err := Config.ReadInConfig()
+	if err != nil {
+		Logger.Fatalf("failed to read config: %v", err)
+	}
+
+	s := Config.GetString("storage")
+	switch s {
+	case "memory":
+		Storage = storage.NewMemoryStorage()
+		break
+	case "redis":
+		Storage = storage.NewRedisStorage(&redis.Options{
+			Addr: fmt.Sprintf("%s:%d", Config.GetString("redis.host"), Config.GetInt("redis.port")),
+			DB:   Config.GetInt("redis.db"),
+		})
+		break
+	default:
+		Logger.Fatalf("unknown storage: %s", s)
+	}
+}
