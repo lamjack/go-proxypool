10b54526f25a09af4d78351bc5b96f8a3d506df4
jack <jack@wizmacau.com>
2023-07-04 16:19:10 +0800

    feat: Add endpoint to retrieve a random proxy from the storage in api.go

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,8 +3,10 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"go-proxypool/pkg/global"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type Server struct {
@@ -27,6 +29,24 @@ func (s *Server) Run() error {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to retrieve proxy IPs"})
 		}
 		c.JSON(http.StatusOK, allProxies)
+	})
+
+	r.GET("/random-proxy", func(c *gin.Context) {
+		allProxies, err := global.Storage.GetAll(c)
+		if err != nil {
+			global.Logger.Errorf("unable to retrieve all proxies: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to retrieve proxy IPs"})
+			return
+		}
+
+		if len(allProxies) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "No proxies available"})
+			return
+		}
+
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		randomIndex := r.Intn(len(allProxies))
+		c.JSON(http.StatusOK, allProxies[randomIndex])
 	})
 
 	return r.Run(":" + strconv.Itoa(s.port))
